d4f442a85908884cf56369d82d9eac5dee3dd96b
David Dataram <daviddataram@Davids-MacBook-Pro-2.local>
2018-10-29 17:01:42 -0400

    Week 4

--- routes/api.js
+++ routes/api.js
@@ -3,15 +3,11 @@ var router = express.Router();
 var bookController = require('../controllers/bookController.js');
 
 /* POST create a new book */
-router.post('/createBook', bookController.addBook);
 
 /* POST retrieve all books */
-router.post('/retrieveBooks', bookController.retrieveBooks);
 
 /* POST delete a book */
-router.post('/deleteBook', bookController.deleteBook);
 
 /* POST update a book */
-router.post('/updateBook', bookController.updateBook);
 
 module.exports = router;
